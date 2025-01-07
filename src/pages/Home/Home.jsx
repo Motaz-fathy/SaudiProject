5db3b07c1c51d177e0baceee5440b6165d6bba30
Motaz-fathy <wazaxxp@gmail.com>
2025-01-07 14:56:05 +0200

    handle errors .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import Navbar from '../../TestComponent/Navbar/Navbar'
-import Hero from '../../TestComponent/Hero'
-import Services from '../../TestComponent/Services'
-import News from '../../TestComponent/News'
-import Statistics from '../../TestComponent/Statistics'
-import RelatedLinks from '../../TestComponent/RelatedLinks'
-import Footer from '../../TestComponent/Footer'
-import Publications from '../../TestComponent/Publications'
-import AddedForms from '../../TestComponent/AddedForms'
+import Navbar from "../../TestComponent/NavBar/Navbar";
+import Hero from "../../TestComponent/Hero";
+import Services from "../../TestComponent/Services";
+import News from "../../TestComponent/News";
+import Statistics from "../../TestComponent/Statistics";
+import RelatedLinks from "../../TestComponent/RelatedLinks";
+import Footer from "../../TestComponent/Footer";
+import Publications from "../../TestComponent/Publications";
+import AddedForms from "../../TestComponent/AddedForms";
 const Home = () => {
   return (
     <div className=" w-full bg-green-900">
@@ -20,7 +20,7 @@ const Home = () => {
       <RelatedLinks />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
